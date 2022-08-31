7e8c70603ca43a0c25edeaa0541d9323dc53152e
UAVXP <uavxp29@gmail.com>
2022-08-31 23:53:41 +0300

    Update installshit4.bat

--- installshit4.bat
+++ installshit4.bat
@@ -2,10 +2,13 @@
 
 rem Development packages
 
+rem Visual Studio C++/C# stuff
 choco install -y visualstudio2022community
 choco install -y visualstudio2022-workload-nativedesktop --package-parameters "--add Microsoft.VisualStudio.Component.VC.ATLMFC --remove Component.Microsoft.VisualStudio.LiveShare.2022;Microsoft.VisualStudio.Component.VC.TestAdapterForBoostTest;Microsoft.VisualStudio.Component.VC.TestAdapterForGoogleTest;Microsoft.VisualStudio.Component.IntelliCode"
 choco install -y visualstudio2022-workload-manageddesktop --package-parameters "--remove Microsoft.VisualStudio.Component.DotNetModelBuilder"
-choco install -y github-desktop
+
+rem Git stuff
+choco install -y github-desktop tortoisegit
 
 pause
 exit
